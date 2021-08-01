c6cf8c2f16417ec2566efd613c99c5237145f332
Arian <84605909+Ary324@users.noreply.github.com>
2021-08-01 05:25:35 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const newTask = function(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+  return task;
+};
+
+// DRIVER CODE CHANGES BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Done the Laundry", "😨");
+const tasks = [task1, task2];
+console.log(tasks);
+
+task1.logState(); // Clean Cat Litter has not been completed
+task2.markCompleted();
+task2.logState();
